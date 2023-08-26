e811f35ddce509fcabbf54ae495144c857d6605d
mwombeki6 <hackerlubex@protonmail.com>
2023-08-26 22:47:59 +0300

    commit

--- tutor-nodb/src/bin/tutor-service.rs
+++ tutor-nodb/src/bin/tutor-service.rs
@@ -2,3 +2,9 @@ use actix_web::{web, App, HttpServer};
 use std::io;
 use std::sync::Mutex;
 
+#[path ="../handlers.rs"]
+mod handlers;
+#[path ="../routes.rs"]
+mod routes;
+#[path ="../state.rs"]
+mod state;

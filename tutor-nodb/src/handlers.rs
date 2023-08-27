06b5eb2736df16c9b6d6e71118f01e39c7d5da41
mwombeki6 <hackerlubex@protonmail.com>
2023-08-28 00:30:37 +0300

    add Handler function to get course detail

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -56,6 +56,27 @@ pub async fn get_courses_for_tutor(
     }
 }
 
+pub async fn get_course_detail(
+    app_state: web::Data<AppState>,
+    params: web::Path<(i32, i32)>,
+) -> HttpResponse {
+    let (tutor_id, course_id) = params.into_inner();
+    let selected_course = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .find(|x| x.tutor_id == tutor_id && x.course_id == Some(course_id))
+        .ok_or("Course not found");
+
+    if let Ok(course) = selected_course {
+        HttpResponse::Ok().json(course)
+    } else {
+        HttpResponse::Ok().json("Course not found".to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;

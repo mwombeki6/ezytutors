2fdef1f5f223bbe58d69e2efaf52bf3a97c6fbe2
mwombeki6 <hackerlubex@protonmail.com>
2023-08-27 23:30:28 +0300

    add function course_routes

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -4,3 +4,8 @@ use actix_web::web;
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
 }
+
+pub fn course_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+    .service(web::scope("/courses"));
+}

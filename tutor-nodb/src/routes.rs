6bae3e13ee8759b219840386a6ce91cbf30cfce4
mwombeki6 <hackerlubex@protonmail.com>
2023-08-28 00:20:31 +0300

    add get_courses_for_tutor

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -7,5 +7,8 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/courses"))
-        .route("/", web::post().to(new_course));
+        .route("/", web::post().to(new_course)
+        .route("/{tutor_id}", web::get().to(get_courses_for_tutor))
+        .route("/{tutor_id}/{course_id}", web::get().to(get_course_detail)),
+    );
 }

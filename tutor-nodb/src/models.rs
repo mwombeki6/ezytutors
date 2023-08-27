94f7e06d1d507c72c6f0ec51fbfaad4d546e82fc
mwombeki6 <hackerlubex@protonmail.com>
2023-08-27 22:24:37 +0300

    implement struct course

--- tutor-nodb/src/models.rs
+++ tutor-nodb/src/models.rs
@@ -9,3 +9,13 @@ pub struct Course {
     pub course_name: String,
     pub posted_time: Option<NaiveDateTime>,
 }
+impl From<web::Json<Course>> for Course {
+    fn from(course: web::Json<Course>) -> Self {
+        Course {
+            tutor_id: course.tutor_id,
+            course_id: course.course_id,
+            course_name: course.course_name.clone(),
+            posted_time: course.posted_time,
+        }
+    }
+}

9a61c67a17b91c7495d7856ed0f67f5f339a2289
mwombeki6 <hackerlubex@protonmail.com>
2023-08-27 23:45:44 +0300

    add function new_course

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -11,3 +11,25 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
     HttpResponse::Ok().json(&response)
 }
 
+pub async fn new_course(
+    new_course: web::Json<Course>,
+    app_state: web::Data<AppState>,
+) -> HttpResponse {
+    println!("Received new course");
+    let course_count_for_user = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.tutor_id == new_course.tutor_id)
+        .count();
+    let new_course = Course {
+        tutor_id: new_course.tutor_id,
+        course_id: Some((course_count_for_user + 1).try_into().unwrap()),
+        course_name: new_course.course_name.clone(),
+        posted_time: Some(Utc::now().naive_utc()),
+    };
+    app_state.courses.lock().unwrap().push(new_course);
+    HttpResponse::Ok().json("Added course")
+}

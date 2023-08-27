7c8ee557ee935c1af594155dde32716fafecd0c7
mwombeki6 <hackerlubex@protonmail.com>
2023-08-27 23:33:36 +0300

    commit course_routes

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -1,11 +1,11 @@
 use super::handlers::*;
-use actix_web::web;
+use actix_web::web::{self, route};
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
 }
 
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
-    cfg
-    .service(web::scope("/courses"));
+    cfg.service(web::scope("/courses"))
+        .route("/", web::post().to(new_course));
 }

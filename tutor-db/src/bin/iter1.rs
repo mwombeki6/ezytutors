ef873f7c606eacd81a8ebfbedca62b2db6b50177
mwombeki6 <hackerlubex@protonmail.com>
2023-08-28 23:55:27 +0300

    add main function

--- tutor-db/src/bin/iter1.rs
+++ tutor-db/src/bin/iter1.rs
@@ -11,3 +11,17 @@ pub struct Course {
     pub course_name: String,
     pub posted_time: Option<NaiveDateTime>,
 }
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    let course_rows = sqlx::query!(
+        r#"select course_id, tutor_id, course_name, posted_time from ezy_course_c4 where course_id = $1"#,
+        1
+    )
+    .fetch_all(&db_pool)
+    .await
+    .unwrap();
+}

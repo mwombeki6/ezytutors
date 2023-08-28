dede35fde46ce513b01faabc2fcbb366f4fdc0d8
mwombeki6 <hackerlubex@protonmail.com>
2023-08-28 23:52:16 +0300

    add Course struct

--- tutor-db/src/bin/iter1.rs
+++ tutor-db/src/bin/iter1.rs
@@ -3,3 +3,11 @@ use std::env;
 use std::io;
 use sqlx::postgres::PgPool;
 use chrono::NaiveDateTime;
+
+#[derive(Debug)]
+pub struct Course {
+    pub course_id: i32,
+    pub tutor_id: i32,
+    pub course_name: String,
+    pub posted_time: Option<NaiveDateTime>,
+}

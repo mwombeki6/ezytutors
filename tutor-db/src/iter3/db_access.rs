7a043c772920cc95555ae0b04fd637e3dd0ea6fb
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 20:16:49 +0300

    add db_access.rs

--- tutor-db/src/iter3/db_access.rs
+++ tutor-db/src/iter3/db_access.rs
@@ -1,2 +1,23 @@
 use super::models::Course;
 use sqlx::postgres::PgPool;
+
+pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Course> {
+    // Prepare SQL statement
+    let course_rows = sqlx::query!(
+        "SELECT tutor_id, course_id, course_name, posted_time FROM ezy_course_c4 where tutor_id = $1",
+        tutor_id
+    )
+    .fetch_all(pool)
+    .await
+    .unwrap();
+    // Extract result
+    course_rows
+        .iter()
+        .map(|course_row| Course {
+            course_id: course_row.course_id,
+            tutor_id: course_row.tutor_id,
+            course_name: course_row.course_name.clone(),
+            posted_time: Some(chrono::NaiveDateTime::from(course_row.posted_time.unwrap())),
+        })
+        .collect()
+}

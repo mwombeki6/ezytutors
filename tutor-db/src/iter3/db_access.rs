846ae46a7b16eb90184b04081d69756bafe8cbd8
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 20:40:17 +0300

    add function post_new_course_db function

--- tutor-db/src/iter3/db_access.rs
+++ tutor-db/src/iter3/db_access.rs
@@ -39,3 +39,27 @@ pub async fn get_course_details_db(pool: &PgPool, tutor_id: i32, course_id: i32)
         posted_time: Some(chrono::NaiveDateTime::from(course_row.posted_time.unwrap())),
     }
 }
+
+pub async fn post_new_course_db(pool: &PgPool, new_course: Course) -> Course {
+    let course_row = sqlx::query!(
+        "insert into ezy_course_c4 (course_id,tutor_id, course_name)  
+                                  values ($1,$2,$3) returning 
+                                  tutor_id, 
+                                  course_id, 
+                                  course_name,  
+                                  posted_time",
+        new_course.course_id,
+        new_course.tutor_id,
+        new_course.course_name
+    )
+    .fetch_one(pool)
+    .await
+    .unwrap();
+    // Retrieve result
+    Course {
+        course_id: course_row.course_id,
+        tutor_id: course_row.tutor_id,
+        course_name: course_row.course_name.clone(),
+        posted_time: Some(chrono::NaiveDateTime::from(course_row.posted_time.unwrap())),
+    }
+}

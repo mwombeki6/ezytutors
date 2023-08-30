f271fba4848d8fe53ec1a1a55209bf46832a2177
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 21:37:04 +0300

    add function health_check_handler

--- tutor-db/src/iter3/handlers.rs
+++ tutor-db/src/iter3/handlers.rs
@@ -4,3 +4,11 @@ use super::state::AppState;
 
 use actix_web::{web, HttpResponse};
 use std::convert::TryFrom;
+
+pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
+    let health_check_response = &app_state.health_check_response;
+    let mut visit_count = app_state.visit_count.lock().unwrap();
+    let response = format!("{} {} times", health_check_response, visit_count);
+    *visit_count += 1;
+    HttpResponse::Ok().json(&response)
+}

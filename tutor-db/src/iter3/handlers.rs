c769d260720cab482b6734c9f07287a54242b52b
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 23:43:41 +0300

    add tests in handlers.rs

--- tutor-db/src/iter3/handlers.rs
+++ tutor-db/src/iter3/handlers.rs
@@ -43,3 +43,65 @@ pub async fn post_new_course(
 
     HttpResponse::Ok().json(course)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use chrono::NaiveDate;
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPool;
+    use std::env;
+    use std::sync::Mutex;
+
+    #[actix_rt::test]
+    async fn get_all_courses_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool,
+        });
+        let tutor_id: web::Path<(i32,)> = web::Path::from((1,));
+        let resp = get_courses_for_tutor(app_state, tutor_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_course_detail_test() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool,
+        });
+        let params: web::Path<(i32, i32)> = web::Path::from((1, 1));
+        let resp = get_course_details(app_state, params).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn post_course_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool,
+        });
+        let new_course_msg = Course {
+            course_id: 3,
+            tutor_id: 1,
+            course_name: "Third course".into(),
+            posted_time: Some(NaiveDate::from_ymd(2020, 12, 18).and_hms(05, 40, 00)),
+        };
+        let course_param = web::Json(new_course_msg);
+        let resp = post_new_course(course_param, app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}

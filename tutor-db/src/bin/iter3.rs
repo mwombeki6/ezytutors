be43aac51c92bf3fae262f8d95bb648264598b31
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 23:11:59 +0300

    commit

--- tutor-db/src/bin/iter3.rs
+++ tutor-db/src/bin/iter3.rs
@@ -4,3 +4,17 @@ use sqlx::postgres::PgPool;
 use std::env;
 use std::io;
 use std::sync::Mutex;
+
+#[path = "../iter3/db_access.rs"]
+mod db_access;
+#[path = "../iter3/handlers.rs"]
+mod handlers;
+#[path = "../iter3/models.rs"]
+mod models;
+#[path = "../iter3/routes.rs"]
+mod routes;
+#[path = "../iter3/state.rs"]
+mod state;
+
+use routes::*;
+use state::AppState;

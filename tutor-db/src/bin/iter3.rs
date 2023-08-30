b40007593f1b84fd2ffd547f7ed2a2bfe7cb6f70
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 23:19:38 +0300

    construct AppState

--- tutor-db/src/bin/iter3.rs
+++ tutor-db/src/bin/iter3.rs
@@ -18,3 +18,18 @@ mod state;
 
 use routes::*;
 use state::AppState;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    // Construct App State
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You've already me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+    // Construct app and configure routes
+}

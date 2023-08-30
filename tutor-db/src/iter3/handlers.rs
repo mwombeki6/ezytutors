675dec373d6cef3077625b9a39d297013678a672
mwombeki6 <hackerlubex@protonmail.com>
2023-08-30 23:12:41 +0300

    refactor code

--- tutor-db/src/iter3/handlers.rs
+++ tutor-db/src/iter3/handlers.rs
@@ -3,7 +3,6 @@ use super::models::Course;
 use super::state::AppState;
 
 use actix_web::{web, HttpResponse};
-use std::convert::TryFrom;
 
 pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
     let health_check_response = &app_state.health_check_response;

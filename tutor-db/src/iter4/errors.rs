bf805495915cfca9c03595959f465e9a9b1081b4
mwombeki6 <hackerlubex@protonmail.com>
2023-09-02 17:29:32 +0300

    add EzyTutorError enum

--- tutor-db/src/iter4/errors.rs
+++ tutor-db/src/iter4/errors.rs
@@ -2,3 +2,14 @@ use actix_web::{error, http::StatusCode, HttpResponse, Result};
 use serde::Serialize;
 use sqlx::error::Error as SQLXError;
 use std::fmt;
+
+#[derive(Debug, Serialize)]
+pub enum EzyTutorError {
+    DBError(String),
+    ActixError(String),
+    NotFound(String),
+}
+#[derive(Debug, Serialize)]
+pub struct MyErrorResponse {
+    error_message: String,
+}

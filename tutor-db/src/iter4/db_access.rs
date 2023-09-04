d9ad94f1a6200d45ce2d895b326c909e67521574
mwombeki6 <hackerlubex@protonmail.com>
2023-09-04 14:25:27 +0300

    commit

--- tutor-db/src/iter4/db_access.rs
+++ tutor-db/src/iter4/db_access.rs
@@ -8,17 +8,26 @@ pub async fn get_courses_for_tutor(
 ) -> Result<Vec<Course>, EzyTutorError> {
     // Prepare SQL statement
     let course_rows = sqlx::query!(
-        "SELECT tutor_id, course_id, course_name, posted_time FROM ezy_course_c5 where tutor_id = $1"
+        "SELECT tutor_id, course_id, course_name, posted_time FROM ezy_course_c5 where tutor_id = $1",
         tutor_id
     )
     .fetch_all(pool)
     .await?;
     // Extract result
 
-    let course: Vec<Course> = course_rows
+    let courses: Vec<Course> = course_rows
         .iter()
         .map(|course_row| Course {
             course_id: course_row.course_id,
-            
+            tutor_id: course_row.tutor.id,
+            course_name: course_row.course_name.clone(),
+            posted_time: Some(course_row.posted_time.unwrap()),
         })
+        .collect();
+    match courses.len() {
+        0 => Err(EzyTutorError::NotFound(
+            "Courses not found for tutor".into(),
+        )),
+        _ => Ok(courses)
+    }
 }

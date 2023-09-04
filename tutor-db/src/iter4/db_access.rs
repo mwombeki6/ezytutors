a0adb4f316cc64d91a72c9e94cb67b0208bd8c23
mwombeki6 <hackerlubex@protonmail.com>
2023-09-04 14:54:18 +0300

    refactor code

--- tutor-db/src/iter4/db_access.rs
+++ tutor-db/src/iter4/db_access.rs
@@ -1,6 +1,5 @@
 use super::errors::EzyTutorError;
 use super::models::Course;
-use sqlx::pool;
 use sqlx::postgres::PgPool;
 
 pub async fn get_courses_for_tutor(
@@ -20,7 +19,7 @@ pub async fn get_courses_for_tutor(
         .iter()
         .map(|course_row| Course {
             course_id: course_row.course_id,
-            tutor_id: course_row.tutor.id,
+            tutor_id: course_row.tutor_id,
             course_name: course_row.course_name.clone(),
             posted_time: Some(course_row.posted_time.unwrap()),
         })

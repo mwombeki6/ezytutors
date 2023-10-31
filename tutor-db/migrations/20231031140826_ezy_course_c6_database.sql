4fefc43d1d2ca238fb58b72178b3e42e96e78f8b
mwombeki6 <hackerlubex@protonmail.com>
2023-10-31 17:10:23 +0300

    commit

--- tutor-db/migrations/20231031140826_ezy_course_c6_database.sql
+++ tutor-db/migrations/20231031140826_ezy_course_c6_database.sql
@@ -1,1 +1,30 @@
 -- Add migration script here
+/* Define a specific user for this book's applications */
+/*drop user if exists lubex;
+create user lubex with password 'lubex6';*/
+
+/* Drop tables if they already exist*/
+
+drop table if exists ezy_course_c6;
+
+/* Create tables. */
+/* Note: Don't put a comma after last field */
+
+create table ezy_course_c6
+(
+    course_id serial primary key,
+    tutor_id INT not null,
+    course_name varchar(140) not null,
+    course_description varchar(2000),
+    course_format varchar(30),
+    course_structure varchar(200),
+    course_duration varchar(30),
+    course_price INT,
+    course_language varchar(30),
+    course_level varchar(30),
+    posted_time TIMESTAMP default now()
+
+);
+
+/* Grant privileges to specific user */
+grant all privileges on table ezy_course_c5 to lubex;
